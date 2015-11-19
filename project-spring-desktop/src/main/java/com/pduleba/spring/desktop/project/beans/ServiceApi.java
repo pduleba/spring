0f68f0cdda1e7673b6dd9bd6ec31dba893e925ba
pduleba <pduleba@pgs-soft.com>
2015-11-19 09:39:42 +0100

    Spring example test case

---
+++ project-spring-desktop/src/main/java/com/pduleba/spring/desktop/project/beans/ServiceApi.java
@@ -0,0 +1,7 @@
+package com.pduleba.spring.desktop.project.beans;
+
+public interface ServiceApi {
+
+	String getResult();
+
+}
---
+++ project-spring-desktop/src/main/java/com/pduleba/spring/desktop/project/beans/TimeServiceApi.java
@@ -0,0 +1,7 @@
+package com.pduleba.spring.desktop.project.beans;
+
+public interface TimeServiceApi {
+
+	public String getCurrentDate();
+	
+}
