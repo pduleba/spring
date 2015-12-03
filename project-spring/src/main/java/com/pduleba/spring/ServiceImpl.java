8f7f0edef407f43e97cde91c50c65af4c470e36b
pduleba <pduleba@pgs-soft.com>
2015-12-03 16:02:47 +0100

    Changes

--- project-spring/src/main/java/com/pduleba/spring/ServiceImpl.java
+++ project-spring/src/main/java/com/pduleba/spring/ServiceImpl.java
@@ -1,19 +1,35 @@
 package com.pduleba.spring;
 
+import static java.util.Objects.isNull;
+
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Component
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public @Data class ServiceImpl implements ServiceApi {
 
+	private ServiceImpl service;
 	private String valueA;
 	private String valueB;
-	
+
+	public ServiceImpl(String valueA, String valueB) {
+		super();
+		this.valueA = valueA;
+		this.valueB = valueB;
+		this.service = new ServiceImpl();
+	}
+
+	@Override
+	public String toString() {
+		if (isNull(service)) {
+			return "ServiceImpl [valueA=" + valueA + ", valueB=" + valueB + "]";
+		} else {
+			return "ServiceImpl [valueA=" + valueA + ", valueB=" + valueB + ", service=" + service + "]";
+		}
+	}	
 }
