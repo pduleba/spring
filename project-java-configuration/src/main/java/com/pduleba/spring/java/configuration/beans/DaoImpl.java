4d0d590cc4cc9c8a55f4b01d23037c3f1a66c9ce
pduleba <pduleba@pgs-soft.com>
2015-12-10 15:36:51 +0100

    Changes

--- project-java-configuration/src/main/java/com/pduleba/spring/java/configuration/beans/DaoImpl.java
+++ project-java-configuration/src/main/java/com/pduleba/spring/java/configuration/beans/DaoImpl.java
@@ -1,8 +1,11 @@
 package com.pduleba.spring.java.configuration.beans;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 
+@Component
 public @Data class DaoImpl implements Dao {
 
 }
