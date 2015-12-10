d9ab3a42a17e32772fc2ee92465595c5ecdb1c8b
pduleba <pduleba@pgs-soft.com>
2015-12-10 16:39:03 +0100

    Inject property by spring's PropertyEditor

--- project-spring/src/main/java/com/pduleba/spring/BeanImpl.java
+++ project-spring/src/main/java/com/pduleba/spring/BeanImpl.java
@@ -1,19 +1,25 @@
 package com.pduleba.spring;
 
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Component
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public @Data class BeanImpl implements BeanApi {
 
 	private String valueA;
 	private String valueB;
+	private Resource resource;
+
+	public BeanImpl(String valueA, String valueB) {
+		super();
+		this.valueA = valueA;
+		this.valueB = valueB;
+	}
 
 }
